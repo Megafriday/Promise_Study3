9f0ffb954917e5c984ce4ef1db9ee3aaee5841f7
Megafriday <megafriday@yahoo.co.jp>
2019-10-15 17:28:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 function myPromise(){
 	return new Promise((resolve)=>{
-		resolve('Hello World.'); //resolve の引数に戻したい値
+		resolve('Hello World.');	//resolve の引数に戻したい値
 	});
 }
 
 
 myPromise().then((data)=>{
-	console.log(data);
+	console.log(data);	// Hello World.
 });
